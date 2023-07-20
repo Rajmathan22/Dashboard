9491b378e85358d7d18fec706fdf9bd30335c23c
Rajmathan22 <105310771+Rajmathan22@users.noreply.github.com>
2023-07-20 19:19:43 +0530

    Add files via upload

---
+++ assets/js/hoverable-collapse.js
@@ -0,0 +1,37 @@
+// Wait for the whole page to load
+window.addEventListener('load', function() {
+  // Add 'loaded' class to the body element to hide the preloader
+  document.body.classList.add('loaded');
+});
+
+
+
+(function($) {
+  'use strict';
+  //Open submenu on hover in compact sidebar mode and horizontal menu mode
+  $(document).on('mouseenter mouseleave', '.sidebar .nav-item', function(ev) {
+    var body = $('body');
+    var sidebarIconOnly = body.hasClass("sidebar-icon-only");
+    var sidebarFixed = body.hasClass("sidebar-fixed");
+    if (!('ontouchstart' in document.documentElement)) {
+      if (sidebarIconOnly) {
+        if (sidebarFixed) {
+          if (ev.type === 'mouseenter') {
+            body.removeClass('sidebar-icon-only');
+          }
+        } else {
+          var $menuItem = $(this);
+          if (ev.type === 'mouseenter') {
+            $menuItem.addClass('hover-open')
+          } else {
+            $menuItem.removeClass('hover-open')
+          }
+        }
+      }
+    }
+  });
+  $('.aside-toggler').click(function(){
+    $('.chat-list-wrapper').toggleClass('slide')
+  });
+})(jQuery);
+
